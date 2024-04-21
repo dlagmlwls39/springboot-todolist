03d1ef2bf1d16f71486af52514da9af9a0b8bf5c
heejin <dlagmlwls39@naver.com>
2024-04-21 19:11:08 +0900

    feat: RootTodo CRUD 기능 추가

--- src/main/java/com/todo/todo/controller/RootTodoController.java
+++ src/main/java/com/todo/todo/controller/RootTodoController.java
@@ -1,10 +1,16 @@
 package com.todo.todo.controller;
 
+import com.todo.todo.dto.RootTodoDto;
+import com.todo.todo.entity.RootTodo;
 import com.todo.todo.service.RootTodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/newTodo/*")
+@RequestMapping("/rootTodo/*")
 public class RootTodoController {
 
     private final RootTodoService rootTodoService;
@@ -12,4 +18,33 @@ public class RootTodoController {
     public RootTodoController(RootTodoService rootTodoService) {
         this.rootTodoService = rootTodoService;
     }
+
+    // 새로운 RootTodo 생성
+    @PostMapping("create")
+    public @ResponseBody ResponseEntity createRootTodo(@RequestBody RootTodoDto rootTodoDto) throws Exception {
+        RootTodo results = rootTodoService.createRootTodo(rootTodoDto);
+        return new ResponseEntity<>(results != null ? "success" : "fail", HttpStatus.OK);
+    }
+
+    // RootTodo 목록 조회
+    @GetMapping("get")
+    public @ResponseBody ResponseEntity getRootTodo(Long userId) throws Exception {
+        List<RootTodoDto> rootTodoList = rootTodoService.getRootTodo(userId);
+        return new ResponseEntity<>(rootTodoList, HttpStatus.OK);
+    }
+
+    // RootTodo 수정
+    @PutMapping("update")
+    public @ResponseBody ResponseEntity updateRootTodo(@RequestBody RootTodoDto rootTodoDto) throws Exception {
+        RootTodo results = rootTodoService.updateRootTodo(rootTodoDto);
+        return new ResponseEntity<>(results != null ? "success" : "fail", HttpStatus.OK);
+    }
+
+    // RootTodo 삭제
+    @DeleteMapping("delete")
+    public @ResponseBody ResponseEntity deleteRootTodo(Long rootTodoId) throws Exception {
+        rootTodoService.deleteRootTodo(rootTodoId);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/todo/todo/repository/RootTodoRepository.java
+++ src/main/java/com/todo/todo/repository/RootTodoRepository.java
@@ -4,7 +4,11 @@ import com.todo.todo.entity.RootTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RootTodoRepository extends JpaRepository<RootTodo, Long> {
+
+    List<RootTodo> findByUserId(Long userId);
 
 }
